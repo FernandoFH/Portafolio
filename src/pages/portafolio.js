0ed8a45f09f5f8e4aa8c7edba790ae1385125267
FernandoFH <ffhf.1820@gmail.com>
2020-06-17 17:47:19 -0300

    Gatsby en la mesa

---
+++ src/pages/portafolio.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function portafolio() {
+    return (
+        <div>
+            <h1>Portafolio</h1>
+        </div>
+    )
+}
---
+++ src/pages/sobreMi.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function sobreMi() {
+    return (
+        <div>
+            <h1>Sobre Mi</h1>
+        </div>
+    )
+}
