37b806c6499a193bfec7bb5ba7683752524967d6
FernandoFH <ffhf.1820@gmail.com>
2022-03-15 21:36:48 -0300

    UPDATE: Beta Blog page

---
+++ src/apis/get-post.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { parseString } from 'xml2js';
+
+const POSTS_URL = 'https://medium.com/feed/@FernandoFH';
+
+export default (req, res) => {
+   axios.get(POSTS_URL)
+      .then(response => {
+         parseString(response.data, (err, res2) =>{
+            res.end(JSON.stringify(res2.rss.channel[0].item))
+         })
+      })
+      .catch(response => res.end(response.data));
+}
+console.log("Teste");
+console.log(POSTS_URL);
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,6 +6,8 @@ import styled from "@emotion/styled"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import { data } from "../apis/get-post.js"
+
 const Content = styled.div`
   margin: 0 auto;
   max-width: 860px;
@@ -37,69 +39,8 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <SEO title="Blog" />
-      <Content>
-        <h2>Blogs</h2>
-        {data.allMarkdownRemark.edges
-          .filter(({ node }) => {
-            const rawDate = node.frontmatter.rawDate
-            const date = new Date(rawDate)
-            return date < new Date()
-          })
-          .map(({ node }) => (
-            <div key={node.id}>
-              <Link
-                to={node.frontmatter.path}
-                css={css`
-                  text-decoration: none;
-                  color: inherit;
-                `}
-              >
-                <MarkerHeader>{node.frontmatter.title} </MarkerHeader>
-                <div>
-                  <ArticleDate>{node.frontmatter.date}</ArticleDate>
-                  <ReadingTime> - {node.fields.readingTime.text}</ReadingTime>
-                </div>
-                <p>{node.excerpt}</p>
-              </Link>
-            </div>
-          ))}
-      </Content>
     </Layout>
   )
 }
 
 export default IndexPage
-
-export const query = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(
-      sort: { fields: [frontmatter___date], order: DESC }
-      filter: { frontmatter: { draft: { eq: false } } }
-    ) {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "DD MMMM, YYYY")
-            rawDate: date
-            path
-          }
-          fields {
-            slug
-            readingTime {
-              text
-            }
-          }
-          excerpt
-        }
-      }
-    }
-  }
-`
